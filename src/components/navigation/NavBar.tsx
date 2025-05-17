1ffd1488b0e827a9ea177af91e541d14e97e15e4
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 23:31:50 -0700

    Adds Footer and NavBar components for site navigation
    Introduces new Footer and NavBar components to enhance site navigation and user engagement.
    
    The Footer includes sections for solutions, support, company information, legal links, and social media icons, improving accessibility to important resources. The NavBar provides dropdown menus for engagement and resources, enriching the user experience with easy access to recent posts and company details.
    
    These additions aim to improve site structure and user interaction.

---
+++ src/components/navigation/NavBar.tsx
@@ -0,0 +1,201 @@
+import { recentPosts } from "@/lib/constants/navigation/featured";
+import {
+  community,
+  company,
+  engagement,
+  resources,
+} from "@/lib/constants/navigation/pages";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import { ThemeToggle } from "../button/ThemeToggle";
+
+export default function NavBar() {
+  return (
+    <main className="">
+      <div className="relative z-50 flex justify-between items-center bg-background shadow-sm py-5 w-full isolate">
+        <div className="flex mx-auto w-7/8">
+          <Popover>
+            <div className="bg-background py-5 w-full">
+              <div className="mx-auto px-6 lg:px-8 max-w-7xl">
+                <PopoverButton className="inline-flex items-center gap-x-1 font-semibold text-sm/6">
+                  Solutions
+                  <ChevronDownIcon aria-hidden="true" className="size-5" />
+                </PopoverButton>
+              </div>
+            </div>
+
+            <PopoverPanel
+              transition
+              className="top-0 -z-10 absolute inset-x-0 bg-background data-closed:opacity-0 shadow-lg pt-16 pb-7 border-b ring-1 ring-gray-900/5 transition data-closed:-translate-y-1 data-enter:duration-200 data-leave:duration-150 data-enter:ease-out data-leave:ease-in"
+            >
+              <div className="gap-x-8 gap-y-10 grid grid-cols-1 lg:grid-cols-2 mx-auto px-6 lg:px-8 py-10 w-11/12">
+                <div className="gap-x-6 sm:gap-x-8 grid grid-cols-2">
+                  <div>
+                    <h3>Engagement</h3>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-2">
+                        {engagement.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="flex gap-x-4 py-2 font-semibold text-sm/6"
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className="flex-none size-6"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <h3>Resources</h3>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-2">
+                        {resources.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="flex gap-x-4 py-2 font-semibold text-sm/6"
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className="flex-none size-6"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="gap-10 sm:gap-8 grid grid-cols-1 lg:grid-cols-2">
+                  <h3 className="sr-only">Recent posts</h3>
+                  {recentPosts.map((post) => (
+                    <article
+                      key={post.id}
+                      className="relative flex sm:flex-row flex-col lg:flex-col sm:items-start lg:items-stretch gap-x-8 gap-y-6 max-w-2xl isolate"
+                    >
+                      <div className="relative flex-none">
+                        <Image
+                          alt=""
+                          src={post.imageUrl}
+                          className="rounded-lg w-full sm:h-32 lg:h-auto aspect-2/1 object-cover sm:aspect-video"
+                          width={360}
+                          height={200}
+                        />
+                        <div className="absolute inset-0 rounded-lg ring-1 ring-inset" />
+                      </div>
+                      <div>
+                        <h4 className="mt-2 font-semibold text-sm/6">
+                          <a href={post.href}>
+                            <span className="absolute inset-0" />
+                            {post.title}
+                          </a>
+                        </h4>
+                        <p className="mt-2 text-sm/6">{post.description}</p>
+                      </div>
+                    </article>
+                  ))}
+                </div>
+              </div>
+            </PopoverPanel>
+          </Popover>
+          <Popover>
+            <div className="bg-background py-5 w-full">
+              <div className="mx-auto px-6 lg:px-8 max-w-7xl">
+                <PopoverButton className="inline-flex items-center gap-x-1 font-semibold text-sm/6">
+                  About
+                  <ChevronDownIcon aria-hidden="true" className="size-5" />
+                </PopoverButton>
+              </div>
+            </div>
+
+            <PopoverPanel
+              transition
+              className="top-0 -z-10 absolute inset-x-0 bg-background data-closed:opacity-0 shadow-lg pt-16 pb-7 border-b ring-1 ring-gray-900/5 transition data-closed:-translate-y-1 data-enter:duration-200 data-leave:duration-150 data-enter:ease-out data-leave:ease-in"
+            >
+              <div className="gap-x-8 gap-y-10 grid grid-cols-1 lg:grid-cols-2 mx-auto px-6 lg:px-8 py-10 w-11/12">
+                <div className="gap-x-6 sm:gap-x-8 grid grid-cols-2">
+                  <div>
+                    <h3>Community</h3>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-2">
+                        {community.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="flex gap-x-4 py-2 font-semibold text-sm/6"
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className="flex-none size-6"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <h3>Company</h3>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-2">
+                        {company.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="flex gap-x-4 py-2 font-semibold text-sm/6"
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className="flex-none size-6"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="gap-10 sm:gap-8 grid grid-cols-1 lg:grid-cols-2">
+                  <h3 className="sr-only">Recent posts</h3>
+                  {recentPosts.map((post) => (
+                    <article
+                      key={post.id}
+                      className="relative flex sm:flex-row flex-col lg:flex-col sm:items-start lg:items-stretch gap-x-8 gap-y-6 max-w-2xl isolate"
+                    >
+                      <div className="relative flex-none">
+                        <Image
+                          alt=""
+                          src={post.imageUrl}
+                          className="bg-gray-100 rounded-lg w-full sm:h-32 lg:h-auto aspect-2/1 object-cover sm:aspect-video"
+                          width={360}
+                          height={200}
+                        />
+                        <div className="absolute inset-0 rounded-lg ring-1 ring-gray-900/10 ring-inset" />
+                      </div>
+                      <div>
+                        <h4 className="mt-2 font-semibold text-sm/6">
+                          <a href={post.href}>
+                            <span className="absolute inset-0" />
+                            {post.title}
+                          </a>
+                        </h4>
+                        <p className="mt-2 text-sm/6">{post.description}</p>
+                      </div>
+                    </article>
+                  ))}
+                </div>
+              </div>
+            </PopoverPanel>
+          </Popover>
+        </div>
+        <ThemeToggle />
+      </div>
+    </main>
+  );
+}
