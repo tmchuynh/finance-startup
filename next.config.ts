8d68d97b81a9e86f9239e826f60a90d333cb0cbc
Tina Huynh <tinamchuynh@gmail.com>
2025-05-16 23:31:06 -0700

    Adds remote image hosting support in configuration
    Introduces a list of approved remote image hosts to enhance image loading capabilities.
    
    Updates the Next.js configuration to allow images from specified remote domains, improving flexibility and asset management for the application.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
+const imageHosts = [
+  "images.unsplash.com",
+  "www.research.colostate.edu",
+  "tailwindcss.com",
+  "bremershs.eq.edu.au",
+  "plus.unsplash.com",
+  "img.freepik.com",
+  "seeklogo.com",
+  "images.seeklogo.com",
+  "media2.dev.to",
+  "miro.medium.com",
+  "blogger.googleusercontent.com",
+  "logovectordl.com",
+  "cdn.worldvectorlogo.com",
+  "988lifeline.org",
+  "www.crisistextline.org",
+  "www.thehotline.org",
+  "rainn.org",
+  "play-lh.googleusercontent.com",
+  "c.media-amazon.com",
+  "media.istockphoto.com",
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: imageHosts.map((hostname) => ({
+      protocol: "https",
+      hostname,
+      port: "",
+      pathname: "/**",
+    })),
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
