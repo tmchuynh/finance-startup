8d218ffc09c1f3ff733cf04094d6df421c3e4540
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 14:24:12 -0700

    fix: update layout for Finance Calculator page to improve structure and alignment

--- src/app/finance-calculators/page.tsx
+++ src/app/finance-calculators/page.tsx
@@ -1,9 +1,46 @@
+"use client";
+
+import { financeCalculatorCategories } from "@/lib/constants/calculators/categories";
+import { useRouter } from "next/navigation";
+
 export default function FinanceCalculatorPage() {
+  const router = useRouter();
+  const financeCalculators = financeCalculatorCategories.map((category) => ({
+    id: category.id,
+    title: category.title,
+    description: category.description,
+    introduction: category.introduction,
+    usage: category.usage,
+    list: category.list,
+  }));
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      <div className="p-4 w-full max-w-2xl">
-        <h1 className="font-bold text-2xl">Finance Calculators</h1>
-        <p>Welcome to the finance calculators collection!</p>
+    <div className="flex flex-col justify-center items-center mx-auto mt-10 w-11/12">
+      <h1 className="font-bold text-2xl">Finance Calculators</h1>
+      <p>Welcome to the finance calculators collection!</p>
+
+      <div className="space-y-6">
+        {financeCalculators.map((category) => (
+          <div key={category.id}>
+            <h2
+              className="font-bold text-xl underline-offset-2 hover:underline cursor-pointer"
+              onClick={() => router.push(`/finance-calculators/${category.id}`)}
+            >
+              {category.title}
+            </h2>
+            <p>{category.description}</p>
+            {category.introduction && (
+              <p className="italic">{category.introduction}</p>
+            )}
+            {category.usage && <p>{category.usage}</p>}
+            {category.list && (
+              <ul className="gap-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 pl-5 list-disc">
+                {category.list.map((item) => (
+                  <li key={item.id}>{item.title}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
