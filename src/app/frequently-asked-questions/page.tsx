541bc180f56c2d3fdf36ce4651ba757717743ea5
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 14:03:58 -0700

    Adds FAQ component to display frequently asked questions
    Introduces a new FAQ component that presents frequently asked questions and their answers in a structured format.
    
    The component utilizes a predefined list of FAQs and renders them with appropriate styling to enhance user experience.
    
    This addition aims to improve user navigation by providing clear answers to common queries.

---
+++ src/app/frequently-asked-questions/page.tsx
@@ -0,0 +1,38 @@
+import { faqs } from "@/lib/constants/faq/questions";
+
+export default function FAQs() {
+  return (
+    <div className="flex flex-col justify-center items-center mx-auto w-11/12 h-full">
+      <h1 className="font-bold text-2xl">Frequently Asked Questions</h1>
+      <p>Welcome to the Frequently Asked Questions!</p>
+
+      <section className="mx-auto mt-24 sm:mt-56 px-6 lg:px-8 max-w-7xl">
+        <h2 className="font-semibold text-4xl sm:text-5xl tracking-tight">
+          Frequently asked questions
+        </h2>
+        <dl className="mt-20 divide-y">
+          {faqs.map((faq) => (
+            <div
+              key={faq.id}
+              className="lg:gap-8 lg:grid lg:grid-cols-12 py-8 first:pt-0 last:pb-0"
+            >
+              <dt className="lg:col-span-5 font-semibold">{faq.question}</dt>
+              <dd className="lg:col-span-7 mt-4 lg:mt-0">
+                <p>{faq.answer.text}</p>
+                {faq.answer.list && (
+                  <ul className="mt-4 list-disc list-inside">
+                    {faq.answer.list.map((item) => (
+                      <li key={item} className="text-sm/6">
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </section>
+    </div>
+  );
+}
