dda5357bcda501128e3b0fce195500092dae4c37
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 16:21:50 -0700

    Refactors calculator component for improved clarity
    Renames the BusinessCalculator component to Calculator for consistency.
    
    Removes unnecessary variables and console logs to streamline the code and enhance readability.

--- src/app/finance-calculators/(calculator)/[type]/page.tsx
+++ src/app/finance-calculators/(calculator)/[type]/page.tsx
@@ -4,18 +4,10 @@ import CalculatorCard from "@/components/cards/CalculatorCard";
 import { useCalculatorContext } from "@/context/calculatorContext";
 import { capitalize } from "@/lib/utils/format";
 
-export default function BusinessCalculator() {
-  const {
-    calculatorType,
-    calculatorName,
-    correspondingCalculators,
-    calculatorCategory,
-  } = useCalculatorContext();
+export default function Calculator() {
+  const { calculatorType, correspondingCalculators, calculatorCategory } =
+    useCalculatorContext();
 
-  console.log("calculatorType", calculatorType);
-  console.log("calculatorName", calculatorName);
-  console.log("correspondingCalculators", correspondingCalculators);
-  console.log("calculatorCategory", calculatorCategory);
   return (
     <div className="mx-auto mt-8 md:mt-12 w-11/12 h-full">
       <h1>{capitalize(calculatorType)}</h1>
