4f04cfb532e002013076ff47e4dd0cf90d742e67
Tina Huynh <tinamchuynh@gmail.com>
2025-05-17 21:47:16 -0700

    fix: update icon library to react-icons and clean up image hosts in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,12 @@ import type { NextConfig } from "next";
 
 const imageHosts = [
   "images.unsplash.com",
-  "www.research.colostate.edu",
   "tailwindcss.com",
-  "bremershs.eq.edu.au",
   "plus.unsplash.com",
   "img.freepik.com",
-  "seeklogo.com",
-  "images.seeklogo.com",
-  "media2.dev.to",
-  "miro.medium.com",
-  "blogger.googleusercontent.com",
-  "logovectordl.com",
-  "cdn.worldvectorlogo.com",
-  "988lifeline.org",
-  "www.crisistextline.org",
-  "www.thehotline.org",
   "rainn.org",
-  "play-lh.googleusercontent.com",
-  "c.media-amazon.com",
   "media.istockphoto.com",
+  "cdn.pixabay.com",
 ];
 
 const nextConfig: NextConfig = {
