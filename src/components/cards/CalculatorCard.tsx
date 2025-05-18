20c1458d0d8ba54e3679f58102c5cf8ae4ec0495
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 14:55:26 -0700

    Improves CalculatorCard with routing and structured content
    Enhances the CalculatorCard component by implementing routing functionality that enables dynamic URL generation based on the title.
    
    Refactors the card structure to utilize CardHeader and CardContent for better organization and readability, while removing the unused children prop.
    
    This change facilitates easier navigation and enhances the user experience by allowing direct access to specific card details.

--- src/components/cards/CalculatorCard.tsx
+++ src/components/cards/CalculatorCard.tsx
@@ -1,22 +1,32 @@
 "use client";
-import { Card } from "../ui/card";
+import { usePathname, useRouter } from "next/navigation";
+import { Card, CardContent, CardHeader } from "../ui/card";
 
 export default function CalculatorCard({
   title,
   description,
-  children,
 }: {
   title: string;
   description: string;
-  children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const formatTitle = (title: string) => {
+    return title.toLowerCase().replaceAll(/\s+/g, "-");
+  };
   return (
-    <Card className="flex flex-col justify-center items-center w-full h-full">
-      <div className="p-4 w-full max-w-2xl">
-        <h2 className="font-bold text-xl">{title}</h2>
+    <Card>
+      <CardHeader>
+        <h4
+          className="underline-offset-2 hover:underline cursor-pointer"
+          onClick={() => router.push(`${pathname}/${formatTitle(title)}`)}
+        >
+          {title}
+        </h4>
+      </CardHeader>
+      <CardContent>
         <p>{description}</p>
-        {children}
-      </div>
+      </CardContent>
     </Card>
   );
 }
