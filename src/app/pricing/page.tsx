7029308baf02c253d0751a37fb6b9d087d7cbb2c
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 14:03:46 -0700

    Refactors pricing page to use ToggleGroup for frequency selection
    Replaces the RadioGroup component with a ToggleGroup for selecting payment frequency, improving the user interface and interaction experience.
    
    Updates the layout for better responsiveness and visual consistency.
    
    Enhances button components for trial initiation and tier selection, ensuring a more modern design and better usability.

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { pricingFAQs } from "@/lib/constants/faq/questions";
 import { pricing } from "@/lib/constants/pricing/prices";
-import {
-  Radio,
-  RadioGroup,
-  Tab,
-  TabGroup,
-  TabList,
-  TabPanel,
-  TabPanels,
-} from "@headlessui/react";
-import { CheckIcon, MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { formatNumberToCurrency } from "@/lib/utils/format";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { CheckIcon, MinusIcon } from "@heroicons/react/20/solid";
 import { Fragment, useState } from "react";
 
 export default function Example() {
@@ -26,11 +21,11 @@ export default function Example() {
   });
 
   return (
-    <main>
+    <main className="mx-auto w-11/12">
       {/* Pricing section */}
-      <div className="overflow-hidden isolate">
+      <div className="px-4 overflow-hidden isolate">
         <div className="py-16 sm:pt-32 lg:pb-0 flow-root">
-          <div className="mx-auto px-6 lg:px-8 max-w-7xl">
+          <div className="">
             <div className="relative z-10">
               <h1 className="mx-auto max-w-4xl font-semibold text-5xl text-balance text-center sm:text-6xl tracking-tight">
                 Pricing that grows with you
@@ -41,105 +36,96 @@ export default function Example() {
                 driving sales.
               </p>
               <div className="flex justify-center mt-16">
-                <fieldset aria-label="Payment frequency">
-                  <RadioGroup
-                    value={frequency}
-                    onChange={setFrequency}
-                    className="gap-x-1 grid grid-cols-2/5 p-1 rounded-full font-semibold text-center text-xs/5"
-                  >
-                    {pricing.frequencies.map((option) => (
-                      <Radio
-                        key={option.value}
-                        value={option}
-                        className="data-checked:bg-indigo-500 px-2.5 py-1 rounded-full cursor-pointer"
-                      >
-                        {option.label}
-                      </Radio>
-                    ))}
-                  </RadioGroup>
-                </fieldset>
+                <ToggleGroup
+                  type="single"
+                  defaultValue={frequency.value}
+                  aria-label="Payment frequency"
+                  onValueChange={(value) => {
+                    const selectedFrequency = pricing.frequencies.find(
+                      (freq) => freq.value === value
+                    );
+                    if (selectedFrequency) {
+                      setFrequency(selectedFrequency);
+                      localStorage.setItem(
+                        "pricing-frequency",
+                        JSON.stringify(selectedFrequency)
+                      );
+                    }
+                  }}
+                >
+                  {pricing.frequencies.map((option) => (
+                    <ToggleGroupItem
+                      key={option.value}
+                      value={option.value}
+                      size={"lg"}
+                      className="flex border w-[10em]"
+                    >
+                      {option.label}
+                    </ToggleGroupItem>
+                  ))}
+                </ToggleGroup>
               </div>
             </div>
-            <div className="relative gap-y-8 grid grid-cols-1 lg:grid-cols-4 mx-auto lg:mx-0 mt-10 lg:-mb-14 max-w-md lg:max-w-none">
-              {pricing.tiers.map((tier) => (
-                <div
-                  key={tier.name}
-                  className="grid grid-cols-1 shadow-[inset_0_0_2px_1px_#ffffff4d] -m-2 max-lg:mx-auto rounded-4xl ring-1 ring-black/5 max-lg:w-full max-lg:max-w-md"
-                >
-                  <div className="grid grid-cols-1 shadow-black/5 shadow-md p-2 rounded-4xl">
-                    <div className="bg-white shadow-2xl p-10 pb-9 rounded-3xl ring-1 ring-black/5">
-                      <h2 className="font-semibold text-indigo-600 text-sm">
-                        {tier.name} <span className="sr-only">plan</span>
-                      </h2>
-                      <p className="mt-2 text-gray-600 text-pretty text-sm/6">
-                        {tier.description}
-                      </p>
-                      <div className="flex items-center gap-4 mt-8">
-                        <div className="font-semibold text-5xl text-gray-950">
-                          {tier.priceMonthly}
+            <div className="flex justify-end w-full">
+              <div className="relative items-end gap-8 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 lg:mx-0 mt-10 lg:-mb-14 lg:w-4/5 2xl:w-6/7 xl:w-3/4">
+                {pricing.tiers.map((tier) => (
+                  <div
+                    key={tier.name}
+                    className="grid grid-cols-1 shadow-[inset_0_0_2px_1px_#ffffff4d] -m-2 max-lg:mx-auto rounded-4xl ring-1 ring-ring max-lg:w-full max-lg:max-w-md"
+                  >
+                    <div className="grid grid-cols-1 shadow-black/5 shadow-md p-2 rounded-4xl">
+                      <div className="bg-card shadow-2xl p-10 pb-9 rounded-3xl ring-1 ring-black/5">
+                        <h5>
+                          {tier.name} <span className="sr-only">plan</span>
+                        </h5>
+                        <p className="mt-2 text-pretty text-sm/6">
+                          {tier.description}
+                        </p>
+                        <div className="flex items-center gap-4 mt-8">
+                          <h1 className="font-semibold text-6xl">
+                            {frequency.value === "monthly"
+                              ? formatNumberToCurrency(tier.priceMonthly)
+                              : formatNumberToCurrency(
+                                  Math.round(tier.priceMonthly * 12 * 0.8)
+                                )}
+                          </h1>
+                          <div className="text-sm">
+                            <p>USD</p>
+                            <p>
+                              {frequency.value === "monthly"
+                                ? "per month"
+                                : "per year"}
+                            </p>
+                          </div>
                         </div>
-                        <div className="text-gray-600 text-sm">
-                          <p>USD</p>
-                          <p>per month</p>
+                        <div className="mt-8">
+                          <Button>Start a free trial</Button>
                         </div>
-                      </div>
-                      <div className="mt-8">
-                        <a
-                          href={tier.href}
-                          aria-label={`Start a free trial on the ${tier.name} plan`}
-                          className="inline-block bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2 rounded-md font-semibold text-center text-sm/6 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                          Start a free trial
-                        </a>
-                      </div>
-                      <div className="mt-8">
-                        <h3 className="font-medium text-gray-950 text-sm/6">
-                          Start selling with:
-                        </h3>
-                        <ul className="space-y-3 mt-3">
-                          {tier.highlights.map((highlight) => (
-                            <li
-                              key={highlight.description}
-                              data-disabled={highlight.disabled}
-                              className="group flex items-start gap-4 text-gray-600 text-sm/6 data-disabled:text-gray-400"
-                            >
-                              <span className="inline-flex items-center h-6">
-                                <PlusIcon
-                                  aria-hidden="true"
-                                  className="size-4 fill-gray-400 group-data-disabled:fill-gray-300"
-                                />
-                              </span>
-                              {highlight.disabled ? (
-                                <span className="sr-only">Not included:</span>
-                              ) : null}
-                              {highlight.description}
-                            </li>
-                          ))}
-                        </ul>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
         <div className="relative lg:pt-14">
-          <div className="mx-auto px-6 lg:px-8 pt-16 sm:pt-24 max-w-2xl lg:max-w-7xl">
+          <div className="pt-16 sm:pt-24">
             <table className="max-sm:hidden w-full text-left">
               <caption className="sr-only">Pricing plan comparison</caption>
               <colgroup>
-                <col className="w-2/5" />
-                <col className="w-1/5" />
-                <col className="w-1/5" />
-                <col className="w-1/5" />
+                <col className="w-3/11" />
+                <col className="w-2/11" />
+                <col className="w-2/11" />
+                <col className="w-2/11" />
+                <col className="w-2/11" />
               </colgroup>
               <thead>
                 <tr>
                   <td className="p-0" />
                   {pricing.tiers.map((tier) => (
                     <th key={tier.name} scope="col" className="p-0">
-                      <div className="font-semibold text-indigo-600 text-sm">
+                      <div className="font-semibold text-primary text-sm">
                         {tier.name} <span className="sr-only">plan</span>
                       </div>
                     </th>
@@ -149,13 +135,7 @@ export default function Example() {
                   <td className="p-0" />
                   {pricing.tiers.map((tier) => (
                     <td key={tier.name} className="px-0 pt-3 pb-0">
-                      <a
-                        href={tier.href}
-                        aria-label={`Get started with the ${tier.name} plan`}
-                        className="inline-block bg-white hover:bg-gray-50 shadow-xs px-2.5 py-1.5 rounded-md ring-1 ring-gray-300 ring-inset font-semibold text-gray-900 text-sm"
-                      >
-                        Get started
-                      </a>
+                      <Button variant={"outline"}>Get started</Button>
                     </td>
                   ))}
                 </tr>
@@ -165,10 +145,10 @@ export default function Example() {
                   <tr>
                     <th
                       scope="colgroup"
-                      colSpan={4}
+                      colSpan={6}
                       className="px-0 pt-10 group-first-of-type:pt-5 pb-0"
                     >
-                      <div className="bg-gray-50 -mx-4 px-4 py-3 rounded-lg font-semibold text-gray-950 text-sm/6">
+                      <div className="bg-secondary/20 -mx-4 px-4 py-3 w-full font-semibold text-sm/6">
                         {section.name}
                       </div>
                     </th>
@@ -180,7 +160,7 @@ export default function Example() {
                     >
                       <th
                         scope="row"
-                        className="px-0 py-4 font-normal text-gray-600 text-sm/6"
+                        className="px-0 py-4 font-normal text-sm/6"
                       >
                         {feature.name}
                       </th>
@@ -255,7 +235,7 @@ export default function Example() {
                     </a>
                     {pricing.sections.map((section) => (
                       <Fragment key={section.name}>
-                        <div className="bg-gray-50 -mx-6 mt-10 group-first-of-type:mt-5 px-6 py-3 rounded-lg font-semibold text-gray-950 text-sm/6">
+                        <div className="bg-gray-50 -mx-6 mt-10 group-first-of-type:mt-5 px-6 py-3 rounded-lg font-semibold text-sm/6">
                           {section.name}
                         </div>
                         <dl>
@@ -264,7 +244,7 @@ export default function Example() {
                               key={feature.name}
                               className="grid grid-cols-2 py-4 border-gray-100 border-b last:border-none"
                             >
-                              <dt className="font-normal text-gray-600 text-sm/6">
+                              <dt className="font-normal text-sm/6">
                                 {feature.name}
                               </dt>
                               <dd className="text-center">
