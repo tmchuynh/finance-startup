93de7f5e964ca094e1c841123041131c767ec455
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 14:24:09 -0700

    fix: remove unnecessary margin from TaxCalculator layout for improved alignment

--- src/app/finance-calculators/(calculator)/tax/page.tsx
+++ src/app/finance-calculators/(calculator)/tax/page.tsx
@@ -1,6 +1,6 @@
 export default function TaxCalculator() {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto w-11/12 h-full">
+    <div className="flex flex-col justify-center items-center h-full">
       <h1 className="font-bold text-2xl">Tax Calculators</h1>
       <p>Welcome to the Tax Calculators!</p>
     </div>
