295ef83be222bed647a5cc97d6074aa0cac0c60f
Tina Huynh <tinamchuynh@gmail.com>
2025-05-17 21:24:15 -0700

    feat: update BusinessCalculator and CalculatorLayout for improved clarity and functionality

--- src/app/finance-calculators/(calculator)/business/page.tsx
+++ src/app/finance-calculators/(calculator)/business/page.tsx
@@ -1,8 +1,8 @@
 export default function BusinessCalculator() {
   return (
     <div className="flex flex-col justify-center items-center mx-auto w-11/12 h-full">
-      <h1 className="font-bold text-2xl">Business Calculator</h1>
-      <p>Welcome to the Business Calculator!</p>
+      <h1 className="font-bold text-2xl">Business Calculators</h1>
+      <p>Welcome to the Business Calculators!</p>
     </div>
   );
 }
--- src/app/finance-calculators/(calculator)/layout.tsx
+++ src/app/finance-calculators/(calculator)/layout.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function CalculatorLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const segments = usePathname().split("/");
+  console.log("segments", segments);
+  const calculatorType = segments[segments.length - 1];
+  const calculatorName = segments[segments.length - 2];
+  const calculatorCategory = segments[segments.length - 3];
+  console.log("calculatorType", calculatorType);
+  console.log("calculatorName", calculatorName);
+  console.log("calculatorCategory", calculatorCategory);
+  const calculatorPath = `/${calculatorCategory}/${calculatorName}/${calculatorType}`;
+  console.log("calculatorPath", calculatorPath);
   return (
     <div className="flex flex-col justify-center items-center mx-auto w-11/12 h-full">
       {children}
