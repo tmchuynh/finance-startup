d72b0e0cc5aea541082c75ba542139f783554967
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 16:40:18 -0700

    Removes unused ArticleLayout component and simplifies Article
    Deletes the ArticleLayout component to streamline the codebase.
    
    Updates the Article component by removing unnecessary props and references, enhancing maintainability and clarity.
    
    Future modifications will focus on integrating new features for better user experience.

--- src/app/finance-articles/[article]/page.tsx
+++ src/app/finance-articles/[article]/page.tsx
@@ -1,15 +1,7 @@
-interface ArticleProps {
-  title: string;
-  description: string;
-  introduction?: string;
-}
-
-export default function Article({ params }: { params: { article: string } }) {
-  const { article } = params;
-
+export default function Article() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <div className="p-4 w-full max-w-2xl">{article}</div>
+      <div className="p-4 w-full max-w-2xl"></div>
     </div>
   );
 }
