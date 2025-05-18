c088a87d63f63ae228b1032b103f8484901ce3d0
Tina Huynh <tinamchuynh@gmail.com>
2025-05-17 21:53:13 -0700

    feat: implement BusinessCalculator component with context integration and logging

--- src/app/finance-calculators/(calculator)/business/page.tsx
+++ src/app/finance-calculators/(calculator)/business/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { useCalculatorContext } from "@/context/calculatorContext";
+
 export default function BusinessCalculator() {
+  const { calculatorType, calculatorName, correspondingCalculators } =
+    useCalculatorContext();
+
+  console.log("calculatorType", calculatorType);
+  console.log("calculatorName", calculatorName);
+  console.log("correspondingCalculators", correspondingCalculators);
   return (
     <div className="flex flex-col justify-center items-center mx-auto w-11/12 h-full">
       <h1 className="font-bold text-2xl">Business Calculators</h1>
