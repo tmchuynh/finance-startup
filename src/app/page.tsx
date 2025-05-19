25f079e9a06de465caf5b83b3b712329cd002f09
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 18:40:46 -0700

    Updates hero section text and removes unnecessary elements for a cleaner layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,21 +12,17 @@ import { useRouter } from "next/navigation";
 export default function App() {
   const router = useRouter();
   return (
-    <main className="isolate">
+    <main>
       {/* Hero section */}
       <div className="relative pt-14">
         <div className="py-24 sm:py-32 lg:pb-40">
           <div className="mx-auto px-6 lg:px-8 max-w-7xl">
             <div className="mx-auto max-w-2xl text-center">
-              <h1>Deploy to the cloud with confidence</h1>
+              <h1>Make Every Financial Move Count</h1>
               <p className="mt-8 font-medium text-lg text-pretty sm:text-xl/8">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
                 lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
               </p>
-              <div className="flex justify-center items-center gap-x-6 mt-10">
-                <Button>Get started</Button>
-                <Button variant="outline">Learn more</Button>
-              </div>
             </div>
             <div className="mt-16 sm:mt-24 flow-root">
               <div className="-m-2 lg:-m-4 p-2 lg:p-4 rounded-xl lg:rounded-2xl ring-1 ring-gray-900/10 ring-inset">
@@ -40,18 +36,6 @@ export default function App() {
               </div>
             </div>
           </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="top-[calc(100%-13rem)] sm:top-[calc(100%-30rem)] -z-10 absolute inset-x-0 blur-3xl transform-gpu overflow-hidden"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%+3rem)] sm:left-[calc(50%+36rem)] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 w-144.5 sm:w-288.75 -translate-x-1/2 aspect-1155/678"
-          />
         </div>
       </div>
 
@@ -188,7 +172,7 @@ export default function App() {
       </div>
 
       {/* CTA section */}
-      <div className="relative -z-10 mt-32 px-6 lg:px-8">
+      <div className="relative mt-32 px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="font-semibold text-4xl text-balance sm:text-5xl tracking-tight">
             Take Control of Your Finances Today.
