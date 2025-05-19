bb19fd045618251dd837931f6e6084c577fe072d
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 22:13:08 -0700

    Adds article title to the finance articles page

--- src/app/finance-articles/[article]/page.tsx
+++ src/app/finance-articles/[article]/page.tsx
@@ -1,7 +1,7 @@
 export default function Article() {
   return (
     <div className="mx-auto pt-6 sm:pt-12 lg:pt-16 pb-24 lg:pb-32 w-10/12 md:w-11/12">
-      <h1></h1>
+      <h1>Articles</h1>
     </div>
   );
 }
