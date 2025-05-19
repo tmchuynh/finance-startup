07c6d4e2b53c14c612a1d1102e6f45e7a54d2297
Tina Huynh <tinamchuynh@gmail.com>
2025-05-18 18:03:56 -0700

    Refactors layout of About Us, Our Partners, Our Team, and Contact Us pages to ensure consistent styling with centered content and improved responsiveness.

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,6 +1,6 @@
 export default function ContactUsPage() {
   return (
-    <div className="pt-6 sm:pt-12 lg:pt-16 pb-24 lg:pb-32">
+    <div className="mx-auto pt-6 sm:pt-12 lg:pt-16 pb-24 lg:pb-32 w-11/12">
       <h1>Contact Us</h1>
     </div>
   );
