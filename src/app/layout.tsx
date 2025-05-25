3e52652366f4c710526acd7cff090d04e05c77b4
Tina Huynh <tinamchuynh@gmail.com>
2025-05-24 18:39:10 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import DynamicBreadcrumb from "@/components/navigation/dynamic-breadcrumb";
 import Footer from "@/components/navigation/Footer";
 import NavBar from "@/components/navigation/NavBar";
 import { Providers } from "@/context/providers";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export default function RootLayout({
@@ -15,8 +13,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="relative flex flex-col min-h-screen">
-        <SpeedInsights />
-        <Analytics />
         <Providers>
           <main className="flex-grow pb-24 md:pb-0">
             <NavBar />
