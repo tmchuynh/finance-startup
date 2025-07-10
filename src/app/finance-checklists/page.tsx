f8fb0fbef12427b69ce6397da9483fcfe88f93ea
Tina Huynh <tinamchuynh@gmail.com>
2025-07-10 14:10:26 -0700

    Refactor FinanceChecklistsPage: Add "View All" button for each checklist category; sort checklists by title for improved organization.

--- src/app/finance-checklists/page.tsx
+++ src/app/finance-checklists/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { checklistCategories } from "@/lib/constants/checklists/categories";
+import { sortByProperty } from "@/lib/utils/sort";
 import { useRouter } from "next/navigation";
 
 export default function FinanceChecklistsPage() {
@@ -14,8 +16,10 @@ export default function FinanceChecklistsPage() {
     usage: category.usage,
     list: category.list,
   }));
+
+  const sortedChecklists = sortByProperty(checklists, "title");
   return (
-    <div className="mx-auto pt-6 sm:pt-12 lg:pt-16 pb-24 lg:pb-32 w-10/12 md:w-11/12">
+    <div className="mx-auto pb-24 lg:pb-32 pt-6 sm:pt-12 lg:pt-16 w-10/12 md:w-11/12">
       <h1>Finance Checklists</h1>
       <h5>Explore our collection of finance checklists</h5>
       <p>
@@ -29,14 +33,26 @@ export default function FinanceChecklistsPage() {
       </p>
 
       <div className="space-y-6 mt-8">
-        {checklists.map((category) => (
+        {sortedChecklists.map((category) => (
           <div key={category.id}>
-            <h2
-              className="font-bold text-xl underline-offset-2 hover:underline cursor-pointer"
-              onClick={() => router.push(`/finance-checklists/${category.id}`)}
-            >
-              {category.title}
-            </h2>
+            <div className="flex gap-5 items-center justify-between w-1/4">
+              <h2
+                className="font-bold text-xl underline-offset-2 hover:underline cursor-pointer"
+                onClick={() =>
+                  router.push(`/finance-checklists/${category.id}`)
+                }
+              >
+                {category.title}
+              </h2>
+              <Button
+                onClick={() =>
+                  router.push(`/finance-checklists/${category.id}`)
+                }
+                className="px-4"
+              >
+                View All
+              </Button>
+            </div>
             <p>{category.description}</p>
             {category.introduction && (
               <p className="italic">{category.introduction}</p>
@@ -44,21 +60,23 @@ export default function FinanceChecklistsPage() {
             {category.usage && <p>{category.usage}</p>}
             {category.list && (
               <ul className="gap-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 pl-5 list-disc">
-                {category.list.map((item) => (
-                  <li
-                    key={item.id}
-                    className="underline-offset-2 hover:underline cursor-pointer"
-                    onClick={() =>
-                      router.push(
-                        `/finance-checklists/${category.id}/${item.title
-                          .toLowerCase()
-                          .replaceAll(/\s+/g, "-")}`
-                      )
-                    }
-                  >
-                    {item.title}
-                  </li>
-                ))}
+                {[...category.list]
+                  .sort((a, b) => a.title.localeCompare(b.title))
+                  .map((item) => (
+                    <li
+                      key={item.id}
+                      className="underline-offset-2 hover:underline cursor-pointer"
+                      onClick={() =>
+                        router.push(
+                          `/finance-checklists/${category.id}/${item.title
+                            .toLowerCase()
+                            .replaceAll(/\s+/g, "-")}`
+                        )
+                      }
+                    >
+                      {item.title}
+                    </li>
+                  ))}
               </ul>
             )}
           </div>
