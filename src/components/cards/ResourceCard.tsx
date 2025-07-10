17f2d2933de0362078a6d9d42defb1627d21aabd
Tina Huynh <tinamchuynh@gmail.com>
2025-07-10 14:10:09 -0700

    Refactor ResourceCard: Enhance layout and functionality by adding a button for resource exploration; update title formatting and ensure consistent styling.

--- src/components/cards/ResourceCard.tsx
+++ src/components/cards/ResourceCard.tsx
@@ -1,29 +1,45 @@
 "use client";
-
-import { useRouter } from "next/navigation";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import { formatTitleToKebabCase } from "@/lib/utils/format";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 
 export default function ResourceCard({
   title,
   description,
-  link,
 }: {
   title: string;
-  description?: string;
-  link: string;
+  description: string;
 }) {
   const router = useRouter();
+  const pathname = usePathname();
   return (
-    <Card className="p-4 max-w-2xl">
-      <CardHeader>
-        <h2
-          className="underline-offset-2 hover:underline cursor-pointer"
-          onClick={() => router.push(link)}
+    <Card className="flex flex-col justify-between h-full">
+      <div>
+        <CardHeader>
+          <h3
+            className="underline-offset-2 hover:underline cursor-pointer"
+            onClick={() =>
+              router.push(`${pathname}/${formatTitleToKebabCase(title)}`)
+            }
+          >
+            {title}
+          </h3>
+        </CardHeader>
+        <CardContent>
+          <p>{description}</p>
+        </CardContent>
+      </div>
+      <CardFooter>
+        <Button
+          onClick={() =>
+            router.push(`${pathname}/${formatTitleToKebabCase(title)}`)
+          }
+          className="w-full"
         >
-          {title}
-        </h2>
-      </CardHeader>
-      {description && <CardContent>{description}</CardContent>}
+          Explore Resource
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
