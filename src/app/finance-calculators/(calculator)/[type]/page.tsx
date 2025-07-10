f645afc2a68dddf118c6bedf098fc2314c145ac1
Tina Huynh <tinamchuynh@gmail.com>
2025-07-10 14:10:22 -0700

    Refactor Calculator component: Replace CalculatorCard with ResourceCard; improve error handling for missing calculator type and corresponding calculators; sort calculators by title for better organization.

--- src/app/finance-calculators/(calculator)/[type]/page.tsx
+++ src/app/finance-calculators/(calculator)/[type]/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import CalculatorCard from "@/components/cards/CalculatorCard";
+import ResourceCard from "@/components/cards/ResourceCard";
 import { useCalculatorContext } from "@/context/calculatorContext";
 import { capitalize } from "@/lib/utils/format";
+import { sortByProperty } from "@/lib/utils/sort";
 
 export default function Calculator() {
   const { calculatorType, correspondingCalculators, calculatorCategory } =
     useCalculatorContext();
 
+  if (!calculatorType || !correspondingCalculators) {
+    return (
+      <div className="mt-8 md:mt-12 mx-auto h-full w-10/12 md:w-11/12">
+        <h1>Calculator Not Found</h1>
+        <p>Please select a valid calculator type from the list.</p>
+      </div>
+    );
+  }
+
+  const sortedCalculators = sortByProperty(correspondingCalculators, "title");
+
   return (
-    <div className="mx-auto mt-8 md:mt-12 w-10/12 md:w-11/12 h-full">
+    <div className="mt-8 md:mt-12 mx-auto h-full w-10/12 md:w-11/12">
       <h1>{capitalize(calculatorType)}</h1>
       <p>{calculatorCategory?.introduction}</p>
       <p>{calculatorCategory?.description}</p>
@@ -17,8 +29,8 @@ export default function Calculator() {
       <section>
         {correspondingCalculators && (
           <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
-            {correspondingCalculators.map((calculator, index) => (
-              <CalculatorCard
+            {sortedCalculators.map((calculator, index) => (
+              <ResourceCard
                 key={index}
                 title={calculator.title}
                 description={calculator.description}
