658134185ab519fc708cf581012cf771bf36614d
Tina Huynh <tinamchuynh@gmail.com>
2025-07-10 14:10:30 -0700

    Refactor BusinessCalculator: Replace CalculatorCard with ResourceCard; sort corresponding checklists by title for improved organization.

--- src/app/finance-checklists/(checklist)/[category]/page.tsx
+++ src/app/finance-checklists/(checklist)/[category]/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import CalculatorCard from "@/components/cards/CalculatorCard";
+import ResourceCard from "@/components/cards/ResourceCard";
 import { useChecklistContext } from "@/context/checklistContext";
 import { capitalize } from "@/lib/utils/format";
+import { sortByProperty } from "@/lib/utils/sort";
 
 export default function BusinessCalculator() {
   const { checklistName, checklistCategory, correspondingChecklists } =
     useChecklistContext();
 
-  console.log("correspondingChecklists", correspondingChecklists);
-
   return (
-    <div className="mx-auto mt-8 md:mt-12 w-10/12 md:w-11/12 h-full">
+    <div className="mt-8 md:mt-12 mx-auto h-full w-10/12 md:w-11/12">
       <h1>{capitalize(checklistName)}</h1>
       <p>{checklistCategory?.introduction}</p>
       <p>{checklistCategory?.description}</p>
@@ -19,13 +18,15 @@ export default function BusinessCalculator() {
       <section>
         {correspondingChecklists && (
           <div className="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
-            {correspondingChecklists.map((calculator, index) => (
-              <CalculatorCard
-                key={index}
-                title={calculator.title}
-                description={calculator.description}
-              />
-            ))}
+            {sortByProperty(correspondingChecklists, "title").map(
+              (calculator, index) => (
+                <ResourceCard
+                  key={index}
+                  title={calculator.title}
+                  description={calculator.description}
+                />
+              )
+            )}
           </div>
         )}
       </section>
